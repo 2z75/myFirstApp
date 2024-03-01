da43a2497ece8739c51ef614574adf4b8cc69c60
2z75 <zinedine.chader@gmail.com>
2024-03-01 15:51:10 +0100

    quete 3.1

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
 
   isVisible: boolean = true;
+  isAdmin: boolean = true;
 
   user = {
     name : 'Doe',
@@ -22,5 +24,9 @@ export class UserProfileComponent {
 
   toggleIsVisible(): void {
     this.isVisible = !this.isVisible;
+  }
+
+  changeStatus(): void {
+    this.isAdmin = !this.isAdmin;
   }
 }
