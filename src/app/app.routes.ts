f308c4922ef2b080068f39d4c5507316e18b2de4
2z75 <zinedine.chader@gmail.com>
2024-04-02 01:39:31 +0200

    quete 05 forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignupComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: UserProfileComponent},
+    { path: '', component: HomeComponent},
     { path: 'user', component: UserProfileComponent},
-    { path: 'signup', component: SignUpComponent},
+    { path: 'signup', component: SignupComponent},
 ];
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../models/user';
 
 @Component({
-  selector: 'app-sign-up',
-  standalone: true,
-  imports: [],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+    selector: 'app-signup',
+    standalone: true,
+    imports: [FormsModule, CommonModule],
+    templateUrl: './sign-up.component.html',
+    styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
+export class SignupComponent {
 
+newUser: User  = {
+    email: '',
+    firstname: '',
+    lastname: '',
+    password: '',
+};
+
+formSubmitted = false;
+
+onSubmit() {
+    if (this.formSubmitted) {
+        console.log('Le formulaire a bien été soumis', this.newUser);
+    }
+}
+
+resetForm() {
+    this.formSubmitted = false;
+    this.newUser = {
+        email: '',
+        firstname: '',
+        lastname: '',
+        password: '',
+    };
+}
 }
